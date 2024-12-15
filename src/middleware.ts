b0aa06f95cf88c906126aff7f59fb91b0dae2bef
Thimisul <thimisull@gmail.com>
2024-12-15 10:20:30 -0300

    Atualiza middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-    const token = request.cookies.get('authjs.session-token')
-    if (request.nextUrl.pathname.startsWith('/user/login') && token) {
-        return NextResponse.redirect(new URL('/app/user', request.url))
-    }
-    
-    if (request.nextUrl.pathname.startsWith('/app') && !token) {
-        return NextResponse.redirect(new URL('/user/login', request.url))
-    }
+export function middleware(request : NextRequest) {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  const cookieName = isProduction
+    ? "__Secure-authjs.session-token"
+    : "authjs.session-token";
+
+  const token = request.cookies.get(cookieName);
+
+  if (request.nextUrl.pathname.startsWith("/user/login") && token) {
+    return NextResponse.redirect(new URL("/app/user", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/app") && !token) {
+    return NextResponse.redirect(new URL("/user/login", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-      '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
-  }
+  matcher: ["/app/:path*", "/user/login"],
+};
